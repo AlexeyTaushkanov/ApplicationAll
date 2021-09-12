f4a6e426e881adc39d51ea5f71efe1f73028754d
AlexeyTaushkanov <snipon@mail.ru>
2021-09-12 22:58:38 +0300

    Revision of the UI activity movie details

--- app/src/main/java/com/taushkanov/applicationall/MainActivity.kt
+++ app/src/main/java/com/taushkanov/applicationall/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.taushkanov.applicationall
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.first_activity_text_view)
+
+        textView.setOnClickListener {moveToNextScreen()}
     }
+
+    private fun moveToNextScreen(){
+        val intent = Intent(this,MovieDetailsActivity::class.java)
+        startActivity(intent)
+    }
+
 }
